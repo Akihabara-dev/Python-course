76b612019a3e7dcd14c26429413b6d6974976c31
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 17:04:07 -0300

    Calculate area and perimeter

---
+++ Area and perimeter.py
@@ -0,0 +1,16 @@
+# English: Basic syntax
+# 1. Calculate area and perimeter
+
+# Spanish: Sintaxis basica
+# Calcular área y perímetro.
+
+
+
+length = 5
+width =2
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print ("The Area is: ", area)
+print ("The Perimeter is : ", perimeter)
--- Implicit and explicit data value.py
+++ Implicit and explicit data value.py
@@ -1,8 +1,8 @@
-# English: 
+# English: Type conversion
 # 1. Implicit keeps the numeric format
 # 2. Explicit modifies the format to INT generating a loss of information
 
-# Spanish: 
+# Spanish: Conversión de tipos
 # 1. Implícito mantiene el formato numerico
 # 2. Explícito modifica el formato a entero generando una perdidad de información
 
