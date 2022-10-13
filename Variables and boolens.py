86678c1300ac68b77cf891272e7b9513a6760835
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 16:43:48 -0300

    Value comparison expecting a true or false value

---
+++ Variables and boolens.py
@@ -0,0 +1,12 @@
+# English: 
+# 1. Variable declaration and assignment
+# 2. Value comparison expecting a true or false value
+
+# Spanish: 
+# 1. Declaración y asignación de variable
+# 2. Comparación de valores esperando un valor verdadero o falso
+
+var = 10
+
+print (0 < var < 20)
+print (20 < var < 0)
