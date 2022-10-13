d118c2592b120bb56fb545d1febd4c28e5a905b6
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 16:55:37 -0300

    Type conversion

---
+++ Implicit and explicit data value.py
@@ -0,0 +1,13 @@
+# English: 
+# 1. Implicit keeps the numeric format
+# 2. Explicit modifies the format to INT generating a loss of information
+
+# Spanish: 
+# 1. Implícito mantiene el formato numerico
+# 2. Explícito modifica el formato a entero generando una perdidad de información
+
+a = 10
+b = 5.5
+
+print (a + b)
+print ((a) + int(b))
