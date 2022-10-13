4fc4d317c8b758f7dab3bc838603bf7f0f2f274f
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 17:44:39 -0300

    Input() with arguments and Input() with no arguments

---
+++ Input.py
@@ -0,0 +1,14 @@
+# English: To read in Python is simple, just use the input() function.
+# 1. Input with arguments
+# 2. Input with no arguments
+
+# Spanish: Leer en Python es simple, solo usa la función input().
+# 1. Entrada con argumentos
+# 2. Entrada sin argumentos
+
+
+x = input("Enter x value: ")
+print ("The value of x is: ",x)
+
+y = input()
+print ("The value of y is: ",y)
