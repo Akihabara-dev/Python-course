57d95c11a913c2c9aea60f145accc45c112ca2f9
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 22:08:26 -0300

    You want to display the numbers 0 through 3 and then show that you have ended with the word Done

---
+++ exercise 3.py
@@ -0,0 +1,11 @@
+# English: 
+# You want to display the numbers 0 through 3 and then show that you have ended with the word "Done".
+
+# Spanish: 
+# Se desea mostrar los nuumeros del 0 al 3 y despuues mostrar que se ha terminado con la palabra "Done".
+
+x = 0
+while (x <= 3):
+    print (x)
+    x = x+1
+print ("Done")
