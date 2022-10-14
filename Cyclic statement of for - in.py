9553c04d651d0321f2ed12372f728d8e64ad5e9c
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 23:02:27 -0300

    Cyclic statement of for - in

---
+++ Cyclic statement of for - in.py
@@ -0,0 +1,6 @@
+# English: Cyclic statement of for - in
+
+# Spanish: Sentencia cíclico de for - in
+
+for name in ["Alexis", "Arnold", "Michael", "William", "Samuel"]:
+    print ("Hello!" , name + ".")
