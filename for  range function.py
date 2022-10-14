284b459a7fa773c52923810d19fc9a7bb40d20a5
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-13 23:13:05 -0300

    It is a function that gives us a series of values. It will allow us to iterate.

---
+++ for  range function.py
@@ -0,0 +1,26 @@
+# English: 
+# It is a function that gives us a series of values.
+# It will allow us to iterate.
+# Example 1 >>> range(number)
+# Example 2 >>> range(start, number)
+# Example 3 >>> range(start, number, step)
+
+# Spanish: 
+# Es una función que nos entrega una serie de valores.
+# Nos va a permitir iterar.
+# Ejemplo 1 >>> range(número)
+# Ejemplo 2 >>> range(inicio, número)
+# Ejemplo 3 >>> range(inicio, número, paso)
+
+for i in range(3):
+    print (i)
+print ("Done")
+
+
+for i in range(10, 15):
+    print (i)
+print ("Done")
+
+for i in range(100, 20, -20):
+    print (i)
+print ("Done")
