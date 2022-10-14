fe31ce8c5dc27516ef7e3c6437f5c023718cbb04
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-14 00:03:28 -0300

    Calculate BMI >>> Body Mass Index

---
+++ BMI - IMC.py
@@ -0,0 +1,10 @@
+# Calculate BMI >>> Body Mass Index
+
+# Calcular IMC >>> Indice de Masa Corporal
+
+height = float(input("Enter you heigth in Meter Example (1.80): "))
+weight = float(input("Enter you weight in Kilograms: "))
+
+BMI = weight/(height*2)
+
+print ("Your BMI is: ",BMI)
