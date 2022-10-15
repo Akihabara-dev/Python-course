c222c563b46c878a1052e32f91e007afbb30f44c
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-15 20:02:50 -0300

    if conditional statement

--- conditional if.py
+++ conditional if.py
@@ -1,4 +1,4 @@
-# English:
+# English: Reading in Python is simple, just use the input() function.
 # 1. In our case, we want to detect the condition a is not equal to 0 and, only in that case, do not perform the calculation of x.
 
 # Spanish: Leer en Python es simple, solo usa la función input().
