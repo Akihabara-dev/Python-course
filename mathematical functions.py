71c13c408d60e3b7fa7ad5c05e0f32511f751086
Akihabara-dev <akihabara.dev@gmail.com>
2022-10-15 20:00:50 -0300

    To use pi and functions you need the math library

---
+++ mathematical functions.py
@@ -0,0 +1,10 @@
+# English: To use pi and functions you need the math library
+# Spanish: Para usar pi y funciones, necesita la biblioteca matemática
+
+from math import*
+
+print(pi)
+print(abs(-3))
+print(cos(0))
+print(sin(pi/2))
+print(sqrt(4))
